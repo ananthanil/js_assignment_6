1d918d3795708dd92216d6dc3119c6563240ad2e
Ananth Krishnan A <ab.ananth97@gmail.com>
2024-01-13 00:03:38 +0530

    math methods completed

---
+++ mathMethod.js
@@ -0,0 +1,26 @@
+
+// console.log('absolute   5 - 9 = ',Math.abs(5-9));
+
+// console.log('without absolute   5 - 9 = ',5-9);
+
+// console.log('55.22 after Ceil : ',Math.ceil(55.22));
+
+// console.log('55.22 after floor : ',Math.floor(55.22));
+
+// console.log('55.22 after round : ',Math.round(55.22));
+
+// console.log('55.55 after round : ',Math.round(55.55));
+
+// console.log(' Max : ',Math.max(15,1,2,22,150,55));
+
+// console.log(' min : ',Math.min(15,1,2,22,150,55));
+
+// console.log(' Power : ',Math.pow(2,3));
+
+// console.log(' Square : ',Math.sqrt(9));
+
+// console.log(' trunc : ',Math.trunc(-11.359));
+
+console.log(' random : ',Math.floor(Math.random() * 10));
+
+
